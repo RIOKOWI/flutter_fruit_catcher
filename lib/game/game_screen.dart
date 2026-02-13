a9a485eca512a51d39f2019c9c00913c11e23919
RIOKOWI <achyarrio2004@gmail.com>
2026-02-13 12:33:41 +0700

    refactor: wrap stack ke expanded

--- lib/game/game_screen.dart
+++ lib/game/game_screen.dart
@@ -8,47 +8,56 @@ class GameScreen extends StatefulWidget {
 }
 
 class _GameScreenState extends State<GameScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(    
-      body: Stack(
-        children: [
-          Positioned(
-            top: 50,
-            left: 20,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius : BorderRadius.circular(10)
+      body: Expanded(
+        child: Stack(
+          children: [
+            Positioned(
+              top: 50,
+              left: 20,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius : BorderRadius.circular(10)
+                ),
+                child: Text(
+                  'Score 1',
+                  style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold    
+                              ),
+                )
               ),
-              child: Text(
-                'Score 1',
-                style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold    
-                            ),
-              )
             ),
-          ),
-          Positioned(
-            top: 50,
-            right: 20,
-            child: Row(
-              children: [
-                IconButton(
-                  icon: Icon(Icons.music_note),
-                  onPressed: () {}, 
-                ),
-                IconButton(
-                  icon: Icon(Icons.volume_up),
-                  onPressed: () {}, 
-                ),
-              ],
+            Positioned(
+              top: 50,
+              right: 20,
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.music_note),
+                    onPressed: () {}, 
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.volume_up),
+                    onPressed: () {}, 
+                  ),
+                ],
+              )
             )
-          )
-        ],
+          ],
+        ),
       )
     );
   }
