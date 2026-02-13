7e55b7df2847008935fe79f25ca9ffc9df52ca86
RIOKOWI <achyarrio2004@gmail.com>
2026-02-13 19:16:02 +0700

    refactor: move code at file game screen to file main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:game/game/game_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +10,91 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fruit Catcher',
+      title: 'Flutter Game',
       home: GameScreen(),
+    );
+  }
+}
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key});
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  final ValueNotifier<int> counter = ValueNotifier(1);
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(   
+      body: Column(
+        children: [
+          Expanded(
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 50,
+                  left: 20,
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius : BorderRadius.circular(10)
+                    ),
+                    child: ValueListenableBuilder<int>(
+                      valueListenable: counter,
+                      builder: (context, score, child) {
+                        return Text(
+                          'Score: $score',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold
+                          ),
+                        );
+                      },
+                    )
+                  ),
+                ),
+                Positioned(
+                  top: 50,
+                  right: 20,
+                  child: Row(
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.music_note),
+                        onPressed: () {}, 
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.volume_up),
+                        onPressed: () {}, 
+                      ),
+                    ],
+                  )
+                ),
+                Padding(
+                  padding: EdgeInsets.all(16),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      counter.value++;
+                      print(counter.value);
+                    }, 
+                    child: Text("Tambah Score"),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
