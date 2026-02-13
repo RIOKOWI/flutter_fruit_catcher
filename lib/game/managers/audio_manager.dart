3f48229e2b7df6fec5684e84bcb02d6555f4553e
RIOKOWI <achyarrio2004@gmail.com>
2026-02-13 20:14:50 +0700

    feat: create getter

--- lib/game/managers/audio_manager.dart
+++ lib/game/managers/audio_manager.dart
@@ -8,5 +8,8 @@ class AudioManager {
   double _musicVolume = 0.7;
   double _sfxVolume = 1.0;
 
-
+  bool get isMusicEnabled => _isMusicEnabled;
+  bool get isSfxEnabled => _isSfxEnabled;
+  double get musicVolume => _musicVolume;
+  double get sfxVolume => _sfxVolume;
 }
