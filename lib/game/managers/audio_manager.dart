1a07c10604e137834bb94a763297d1766c3d526c
RIOKOWI <achyarrio2004@gmail.com>
2026-02-13 20:11:13 +0700

    feat: set on/off music and sfx, and set volume music and sfx

--- lib/game/managers/audio_manager.dart
+++ lib/game/managers/audio_manager.dart
@@ -3,7 +3,10 @@ class AudioManager {
   factory AudioManager() => _instance;
   AudioManager._internal();
 
+  bool _isMusicEnabled = true;
+  bool _isSfxEnabled = true;
+  double _musicVolume = 0.7;
+  double _sfxVolume = 1.0;
 
 
-  
 }
