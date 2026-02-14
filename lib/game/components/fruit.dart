66f7f14fd9f70565381e0aba55bdfcddc8a656be
RIOKOWI <achyarrio2004@gmail.com>
2026-02-14 08:56:34 +0700

    feat: create position and hitbox

--- lib/game/components/fruit.dart
+++ lib/game/components/fruit.dart
@@ -8,5 +8,19 @@ import 'basket.dart';
 enum FruiType{apple, banana, orange, strawberry}
 
 class Fruit extends PositionComponent with HasGameRef<FruitCatcherGame>, CollisionCallbacks{
+  final FruiType type;
+  final double fallSpeed = 200;
+  final Random random = Random();
 
+  Fruit({super.position})
+    : type = FruiType.values[Random().nextInt(FruiType.values.length)], super(size: Vector2.all(40));
+
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    anchor = Anchor.center;
+    add(CircleHitbox());
+  }
+  
 }
