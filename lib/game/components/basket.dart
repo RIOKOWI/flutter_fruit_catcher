88324d82f97df7f17ddd260233de24f725331297
RIOKOWI <achyarrio2004@gmail.com>
2026-02-14 08:03:35 +0700

    feat: create class basket

---
+++ lib/game/components/basket.dart
@@ -0,0 +1,6 @@
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+
+class Basket extends PositionComponent with HasGameRef, CollisionCallbacks {
+  Basket() : super
+}
